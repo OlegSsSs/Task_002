bec604999471f8bc1d9cc87c782895ac8f992abe
OlegSs <youremail@yourdomain.comolegs934@gmail.com>
2022-10-05 15:45:39 +0300

    Задача решена

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (n > a)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else 
+{
+    Console.WriteLine("Первое число меньше второго");
+}
